1df448d0751682f5e7d85d08b80f3c79ecaca3a9
jakeemma2012 <tranhuuphuc.k20.cva@gmail.com>
2024-09-30 15:36:07 +0700

    hey

---
+++ WinformEmail/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace WinformEmail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btSend = new System.Windows.Forms.Button();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCaption = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtGmailSend = new System.Windows.Forms.TextBox();
+            this.txtAppPassword = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btSend
+            // 
+            this.btSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSend.Location = new System.Drawing.Point(486, 142);
+            this.btSend.Name = "btSend";
+            this.btSend.Size = new System.Drawing.Size(156, 175);
+            this.btSend.TabIndex = 0;
+            this.btSend.Text = "Send Email";
+            this.btSend.UseVisualStyleBackColor = true;
+            this.btSend.Click += new System.EventHandler(this.btSend_Click);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(126, 139);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(345, 22);
+            this.txtEmail.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 142);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Email Address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Caption";
+            // 
+            // txtCaption
+            // 
+            this.txtCaption.Location = new System.Drawing.Point(126, 166);
+            this.txtCaption.Name = "txtCaption";
+            this.txtCaption.Size = new System.Drawing.Size(345, 22);
+            this.txtCaption.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Message";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(126, 195);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(345, 122);
+            this.txtMessage.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Email Send";
+            // 
+            // txtGmailSend
+            // 
+            this.txtGmailSend.Location = new System.Drawing.Point(126, 44);
+            this.txtGmailSend.Name = "txtGmailSend";
+            this.txtGmailSend.Size = new System.Drawing.Size(345, 22);
+            this.txtGmailSend.TabIndex = 7;
+            // 
+            // txtAppPassword
+            // 
+            this.txtAppPassword.Location = new System.Drawing.Point(126, 72);
+            this.txtAppPassword.Name = "txtAppPassword";
+            this.txtAppPassword.Size = new System.Drawing.Size(345, 22);
+            this.txtAppPassword.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "App Password";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(9, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(215, 29);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Config Gmail Send";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(6, 102);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(246, 29);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Config Gmail Receive";
+            // 
+            // txtClear
+            // 
+            this.txtClear.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtClear.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtClear.Location = new System.Drawing.Point(485, 59);
+            this.txtClear.Name = "txtClear";
+            this.txtClear.Size = new System.Drawing.Size(157, 72);
+            this.txtClear.TabIndex = 10;
+            this.txtClear.Text = "Clear Typing";
+            this.txtClear.UseVisualStyleBackColor = false;
+            this.txtClear.Visible = false;
+            this.txtClear.Click += new System.EventHandler(this.txtClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 331);
+            this.Controls.Add(this.txtClear);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtAppPassword);
+            this.Controls.Add(this.txtGmailSend);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCaption);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.btSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btSend;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCaption;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtGmailSend;
+        private System.Windows.Forms.TextBox txtAppPassword;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button txtClear;
+    }
+}
+
---
+++ WinformEmail/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinformEmail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtClear_Click(object sender, EventArgs e)
+        {
+            txtEmail.Clear();
+            txtCaption.Clear();
+            txtMessage.Clear();
+        }
+
+        private void btSend_Click(object sender, EventArgs e)
+        {
+
+            if (isValidTyping())
+            {
+                if (isValidEmail(txtGmailSend.Text.Trim()))
+                {
+                    if (isValidEmail(txtEmail.Text.Trim()))
+                    {
+                        setOnOffButton(false);
+                        emailSend();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Email is not Valid", "Erorr", MessageBoxButtons.OK);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Email Send is not Valid", "Erorr", MessageBoxButtons.OK);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please Typing The Email Fisrt !");
+            }
+        }
+
+        void emailSend()
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress(txtGmailSend.Text.Trim());
+                mail.To.Add(txtEmail.Text.Trim());
+                mail.Subject = txtCaption.Text;
+                mail.Body = txtMessage.Text;
+
+                SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+                smtpClient.EnableSsl = true;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new NetworkCredential(txtGmailSend.Text.Trim(), txtAppPassword.Text);
+
+                smtpClient.Send(mail);
+                MessageBox.Show("Send SuccessFul to " + txtEmail.Text.Trim());
+                txtClear.Visible = true;
+            }
+            catch (SmtpException ex)
+            {
+                MessageBox.Show($"SmtpException: {ex.Message}");
+                MessageBox.Show($"StatusCode: {ex.StatusCode}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"General Exception: {ex.Message}");
+            }
+            finally
+            {
+                setOnOffButton(true);
+            }
+
+
+        }
+
+        bool isValidEmail(string inputEmail)
+        {
+            string strRegex = @"^[a-zA-Z0-9._%+-]+@gmail\.com$";
+            Regex re = new Regex(strRegex);
+            if (re.IsMatch(inputEmail))
+                return (true);
+            else
+                return (false);
+        }
+
+        bool isValidTyping()
+        {
+            return txtEmail.Text.Trim() != "" && txtCaption.Text.Trim() != "" && txtMessage.Text.Trim() != ""
+                 && txtGmailSend.Text.Trim() != "" && txtAppPassword.Text.Trim() != "";
+        }
+
+        void setOnOffButton(bool typeset)
+        {
+            txtGmailSend.Enabled = typeset;
+            txtAppPassword.Enabled = typeset;
+
+            txtEmail.Enabled = typeset;
+            txtCaption.Enabled = typeset;
+            txtMessage.Enabled = typeset;
+
+            btSend.Enabled = typeset;
+
+        }
+    }
+}
